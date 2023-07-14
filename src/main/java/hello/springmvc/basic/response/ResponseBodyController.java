ae1ebb1ccd0703667cd1f7880869f46fec4a31e1
JongMinCh0i <wonderland314592@gmail.com>
2023-07-14 19:24:27 +0900

    [Rename] 클래스 명 변경 ResponseView -> ResponseBody

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -1,31 +1,55 @@
 package hello.springmvc.basic.response;
 
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+//@Controller
+@RestController
 public class ResponseBodyController {
 
-    @RequestMapping("/response-view-v1")
-    public ModelAndView responseView1() {
-        ModelAndView mav = new ModelAndView("response/hello")
-                .addObject("data", "hello!");
-
-        return mav;
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
     }
 
-    @RequestMapping("/response-view-v2")
-    public String responseView2(Model model) {
-        model.addAttribute("data", "hello!");
-        return "response/hello";
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() throws IOException {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
-    @RequestMapping("/response/hello")
-    public void responseView3(Model model) {
-        model.addAttribute("data", "hello!!");
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
     }
 
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJson1() {
+        HelloData helloData = new HelloData();
+        helloData.setAge(20);
+        helloData.setUsername("userA");
 
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJson2() {
+        HelloData helloData = new HelloData();
+        helloData.setAge(20);
+        helloData.setUsername("userA");
+        return helloData;
+    }
 }
